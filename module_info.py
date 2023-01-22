43df5163d9e9d7d4e0518aa51fecf5797d1a2b1b
wuhaohao1234 <1611499758@qq.com>
2023-01-22 14:03:11 +0800

    init

---
+++ module_info.py
@@ -0,0 +1,7 @@
+
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+# export_dir = "../WOTS/Modules/Native/"
+export_dir = "D:/games/Mount&Blade Warband/Modules/Test Mod/"
+#export_dir = "C:/Program Files (x86)/Mount&Blade Warband/Modules/Native/"
